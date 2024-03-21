7bfcc69749e286883dcfcda0f53aeb700a9a9d9d
Raymond Chen <ahrchen@gmail.com>
2024-03-21 13:01:35 -0700

    Initial Commit

---
+++ TapDragSpatialGestures/TapDragSpatialGesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapDragSpatialGesturesApp.swift
+//  TapDragSpatialGestures
+//
+//  Created by Raymond Chen on 3/21/24.
+//
+
+import SwiftUI
+
+@main
+struct TapDragSpatialGesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
